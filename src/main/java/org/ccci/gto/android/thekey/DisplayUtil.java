4d17d1699836bb0df0c21dae5055b16394c3b5b5
Daniel Frett <daniel.frett@ccci.org>
2013-04-16 10:47:26 -0400

    add some more restrictions to the generated WebView to prevent some potential attack vectors

--- src/main/java/org/ccci/gto/android/thekey/DisplayUtil.java
+++ src/main/java/org/ccci/gto/android/thekey/DisplayUtil.java
@@ -1,14 +1,18 @@
 package org.ccci.gto.android.thekey;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public final class DisplayUtil {
     @SuppressLint("SetJavaScriptEnabled")
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     public final static WebView createLoginWebView(final Context context, final TheKey thekey,
             final LoginWebViewClient webViewClient) {
         final WebView webView = new WebView(context);
@@ -16,8 +20,20 @@ public final class DisplayUtil {
         webView.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
         webView.setScrollbarFadingEnabled(true);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setLoadsImagesAutomatically(true);
+
+        final WebSettings settings = webView.getSettings();
+        settings.setSavePassword(false);
+        settings.setJavaScriptEnabled(true);
+        settings.setLoadsImagesAutomatically(true);
+        settings.setAllowFileAccess(false);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            settings.setAllowContentAccess(false);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                settings.setAllowFileAccessFromFileURLs(false);
+                settings.setAllowUniversalAccessFromFileURLs(false);
+            }
+        }
+
         webView.setWebViewClient(webViewClient);
         webView.loadUrl(thekey.getAuthorizeUri().toString());
         return webView;
