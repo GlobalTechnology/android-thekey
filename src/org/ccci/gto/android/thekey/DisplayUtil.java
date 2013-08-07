c394493eb1d41e1d22aab520b5399e905d22e58f
Daniel Frett <daniel.frett@ccci.org>
2013-08-07 12:55:13 -0400

    add in a workaround for being able to enter text into the login WebView on API level <= 10

--- src/org/ccci/gto/android/thekey/DisplayUtil.java
+++ src/org/ccci/gto/android/thekey/DisplayUtil.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
@@ -13,8 +14,8 @@ import android.webkit.WebView;
 public final class DisplayUtil {
     @SuppressLint("SetJavaScriptEnabled")
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public final static WebView createLoginWebView(final Context context, final TheKey thekey,
-            final LoginWebViewClient webViewClient) {
+    public static WebView createLoginWebView(final Context context, final TheKey thekey,
+                                             final LoginWebViewClient webViewClient) {
         final WebView webView = new WebView(context);
         webView.setVisibility(View.GONE);
         webView.setLayoutParams(new ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
@@ -36,6 +37,37 @@ public final class DisplayUtil {
 
         webView.setWebViewClient(webViewClient);
         webView.loadUrl(thekey.getAuthorizeUri().toString());
+
+        // apply any hacks to work around various bugs in previous versions of android
+        switch (Build.VERSION.SDK_INT) {
+            case Build.VERSION_CODES.FROYO:
+            case Build.VERSION_CODES.GINGERBREAD:
+            case Build.VERSION_CODES.GINGERBREAD_MR1:
+                webView.setOnTouchListener(new SDK8TouchListener());
+                break;
+        }
+
         return webView;
+    }
+
+    /**
+     * This listener is only used on SDK levels 8-10
+     *
+     * This will work around a focus bug affecting WebViews
+     *
+     * see http://stackoverflow.com/questions/3460915
+     */
+    private final static class SDK8TouchListener implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(final View v, final MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                case MotionEvent.ACTION_UP:
+                    if (!v.hasFocus()) {
+                        v.requestFocus();
+                    }
+            }
+            return false;
+        }
     }
 }
