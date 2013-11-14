f048e3d8fd8eee0308b891ad383776c05f9f1f61
Daniel Frett <daniel.frett@ccci.org>
2013-11-14 16:49:29 -0500

    added missing FragmentBuilder

---
+++ thekey-lib/src/me/thekey/android/lib/fragment/FragmentBuilder.java
@@ -0,0 +1,27 @@
+package me.thekey.android.lib.fragment;
+
+import me.thekey.android.lib.AbstractBuilder;
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public final class FragmentBuilder<T extends Fragment> extends AbstractBuilder<T> {
+    private final Class<T> clazz;
+
+    public FragmentBuilder(final Class<T> clazz) {
+        this.clazz = clazz;
+    }
+
+    @Override
+    public final T build() {
+        try {
+            final T fragment = clazz.newInstance();
+            fragment.setArguments(mArgs);
+            return fragment;
+        } catch (final ReflectiveOperationException e) {
+            // propagate exception as a RuntimeException
+            throw new RuntimeException(e);
+        }
+    }
+}
