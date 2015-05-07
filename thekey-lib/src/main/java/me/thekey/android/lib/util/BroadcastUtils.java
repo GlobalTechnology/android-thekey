d3f53c0540437e358d6b20ef5f81d0d6b420b3d7
Daniel Frett <daniel.frett@ccci.org>
2015-05-07 11:20:44 -0400

    add some nonnull annotations

--- thekey-lib/src/main/java/me/thekey/android/lib/util/BroadcastUtils.java
+++ thekey-lib/src/main/java/me/thekey/android/lib/util/BroadcastUtils.java
@@ -5,21 +5,24 @@ import static me.thekey.android.TheKey.ACTION_LOGIN;
 import static me.thekey.android.TheKey.ACTION_LOGOUT;
 import static me.thekey.android.TheKey.EXTRA_CHANGING_USER;
 import static me.thekey.android.TheKey.EXTRA_GUID;
+
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.content.LocalBroadcastManager;
 
 public final class BroadcastUtils {
-    public static void broadcastLogin(final Context context, final String guid) {
+    public static void broadcastLogin(@NonNull final Context context, @NonNull final String guid) {
         LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(ACTION_LOGIN).putExtra(EXTRA_GUID, guid));
     }
 
-    public static void broadcastLogout(final Context context, final String guid, final boolean changingUser) {
+    public static void broadcastLogout(@NonNull final Context context, @NonNull final String guid,
+                                       final boolean changingUser) {
         LocalBroadcastManager.getInstance(context).sendBroadcast(
                 new Intent(ACTION_LOGOUT).putExtra(EXTRA_GUID, guid).putExtra(EXTRA_CHANGING_USER, changingUser));
     }
 
-    public static void broadcastAttributesLoaded(final Context context, final String guid) {
+    public static void broadcastAttributesLoaded(@NonNull final Context context, @NonNull final String guid) {
         LocalBroadcastManager.getInstance(context).sendBroadcast(
                 new Intent(ACTION_ATTRIBUTES_LOADED).putExtra(EXTRA_GUID, guid));
     }
