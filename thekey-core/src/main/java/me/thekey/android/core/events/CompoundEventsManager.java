1215b27e961083f9f59b69cb2881b0e58cef0314
Daniel Frett <daniel.frett@ccci.org>
2018-01-08 14:32:55 -0500

    add a compound events manager to support configuring multiple events managers

---
+++ thekey-core/src/main/java/me/thekey/android/core/events/CompoundEventsManager.java
@@ -0,0 +1,53 @@
+package me.thekey.android.core.events;
+
+import android.support.annotation.NonNull;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import me.thekey.android.events.EventsManager;
+
+public final class CompoundEventsManager implements EventsManager {
+    private final Set<EventsManager> mEventsManagers = new LinkedHashSet<>();
+
+    public CompoundEventsManager(@NonNull final EventsManager... managers) {
+        Collections.addAll(mEventsManagers, managers);
+    }
+
+    public void addEventsManager(@NonNull final EventsManager manager) {
+        mEventsManagers.add(manager);
+    }
+
+    public void removeEventsManager(@NonNull final EventsManager manager) {
+        mEventsManagers.remove(manager);
+    }
+
+    @Override
+    public void loginEvent(@NonNull final String guid) {
+        for (final EventsManager eventsManager : mEventsManagers) {
+            eventsManager.loginEvent(guid);
+        }
+    }
+
+    @Override
+    public void logoutEvent(@NonNull final String guid, final boolean changingUser) {
+        for (final EventsManager eventsManager : mEventsManagers) {
+            eventsManager.logoutEvent(guid, changingUser);
+        }
+    }
+
+    @Override
+    public void changeDefaultSessionEvent(@NonNull final String guid) {
+        for (final EventsManager eventsManager : mEventsManagers) {
+            eventsManager.changeDefaultSessionEvent(guid);
+        }
+    }
+
+    @Override
+    public void attributesUpdatedEvent(@NonNull final String guid) {
+        for (final EventsManager eventsManager : mEventsManagers) {
+            eventsManager.attributesUpdatedEvent(guid);
+        }
+    }
+}
