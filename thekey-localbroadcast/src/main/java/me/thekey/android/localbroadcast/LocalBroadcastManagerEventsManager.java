d3d1b66919c1f0fb4ea7834ba9a171212972647d
Daniel Frett <daniel.frett@ccci.org>
2018-01-08 14:58:47 -0500

    fix checkstyle

--- thekey-localbroadcast/src/main/java/me/thekey/android/localbroadcast/LocalBroadcastManagerEventsManager.java
+++ thekey-localbroadcast/src/main/java/me/thekey/android/localbroadcast/LocalBroadcastManagerEventsManager.java
@@ -7,6 +7,7 @@ import android.support.v4.content.LocalBroadcastManager;
 
 import me.thekey.android.events.EventsManager;
 
+import static me.thekey.android.localbroadcast.BroadcastUtils.theKeyUri;
 import static me.thekey.android.localbroadcast.Constants.ACTION_ATTRIBUTES_LOADED;
 import static me.thekey.android.localbroadcast.Constants.ACTION_CHANGE_DEFAULT_SESSION;
 import static me.thekey.android.localbroadcast.Constants.ACTION_LOGIN;
@@ -24,25 +25,24 @@ public class LocalBroadcastManagerEventsManager implements EventsManager {
 
     @Override
     public void loginEvent(@NonNull final String guid) {
-        mBroadcastManager.sendBroadcast(new Intent(ACTION_LOGIN, BroadcastUtils.theKeyUri(guid)).putExtra(EXTRA_GUID, guid));
+        mBroadcastManager.sendBroadcast(new Intent(ACTION_LOGIN, theKeyUri(guid)).putExtra(EXTRA_GUID, guid));
     }
 
     @Override
     public void logoutEvent(@NonNull final String guid, final boolean changingUser) {
-        mBroadcastManager.sendBroadcast(new Intent(ACTION_LOGOUT, BroadcastUtils.theKeyUri(guid)).putExtra(EXTRA_GUID, guid)
+        mBroadcastManager.sendBroadcast(new Intent(ACTION_LOGOUT, theKeyUri(guid)).putExtra(EXTRA_GUID, guid)
                                                 .putExtra(EXTRA_CHANGING_USER, changingUser));
     }
 
     @Override
     public void changeDefaultSessionEvent(@NonNull final String guid) {
         mBroadcastManager
-                .sendBroadcast(new Intent(ACTION_CHANGE_DEFAULT_SESSION, BroadcastUtils
-                        .theKeyUri(guid)).putExtra(EXTRA_GUID, guid));
+                .sendBroadcast(new Intent(ACTION_CHANGE_DEFAULT_SESSION, theKeyUri(guid)).putExtra(EXTRA_GUID, guid));
     }
 
     @Override
     public void attributesUpdatedEvent(@NonNull final String guid) {
         mBroadcastManager
-                .sendBroadcast(new Intent(ACTION_ATTRIBUTES_LOADED, BroadcastUtils.theKeyUri(guid)).putExtra(EXTRA_GUID, guid));
+                .sendBroadcast(new Intent(ACTION_ATTRIBUTES_LOADED, theKeyUri(guid)).putExtra(EXTRA_GUID, guid));
     }
 }
