6bf11d47f89a6ce33b5eeeeb11ca79bf95a312f9
Daniel Frett <daniel.frett@ccci.org>
2018-05-31 16:03:33 -0400

    support using a raw data URI for processing a code grant

--- thekey-core/src/main/java/me/thekey/android/core/CodeGrantAsyncTask.java
+++ thekey-core/src/main/java/me/thekey/android/core/CodeGrantAsyncTask.java
@@ -8,25 +8,35 @@ import android.support.annotation.Nullable;
 import me.thekey.android.TheKey;
 import me.thekey.android.exception.TheKeyApiError;
 import me.thekey.android.exception.TheKeySocketException;
+import timber.log.Timber;
 
-public abstract class CodeGrantAsyncTask extends AsyncTask<Void, Void, String> {
+import static me.thekey.android.TheKey.PARAM_CODE;
+import static me.thekey.android.TheKey.PARAM_STATE;
+
+public class CodeGrantAsyncTask extends AsyncTask<Void, Void, String> {
     @NonNull
-    protected final TheKey mTheKey;
+    private final TheKey mTheKey;
 
     @NonNull
     private final Uri mRedirectUri;
-    @NonNull
+    @Nullable
     private final String mCode;
     @Nullable
     private final String mState;
 
-    protected CodeGrantAsyncTask(@NonNull final TheKey thekey, @NonNull final String code,
-                                 @Nullable final String state) {
+    public CodeGrantAsyncTask(@NonNull final TheKey thekey, @NonNull final Uri dataUri) {
+        mTheKey = thekey;
+        mRedirectUri = DataUriUtils.removeCodeAndState(dataUri);
+        mCode = dataUri.getQueryParameter(PARAM_CODE);
+        mState = dataUri.getQueryParameter(PARAM_STATE);
+    }
+
+    public CodeGrantAsyncTask(@NonNull final TheKey thekey, @NonNull final String code, @Nullable final String state) {
         this(thekey, null, code, state);
     }
 
-    protected CodeGrantAsyncTask(@NonNull final TheKey thekey, @Nullable final Uri redirectUri,
-                                 @NonNull final String code, @Nullable final String state) {
+    public CodeGrantAsyncTask(@NonNull final TheKey thekey, @Nullable final Uri redirectUri, @NonNull final String code,
+                              @Nullable final String state) {
         mTheKey = thekey;
         mRedirectUri = redirectUri != null ? redirectUri : mTheKey.getDefaultRedirectUri();
         mCode = code;
@@ -39,10 +49,14 @@ public abstract class CodeGrantAsyncTask extends AsyncTask<Void, Void, String> {
 
     @Override
     protected final String doInBackground(final Void... params) {
-        try {
-            return mTheKey.processCodeGrant(mCode, mRedirectUri, mState);
-        } catch (final TheKeyApiError | TheKeySocketException e) {
-            return null;
+        if (mCode != null) {
+            try {
+                return mTheKey.processCodeGrant(mCode, mRedirectUri, mState);
+            } catch (final TheKeyApiError | TheKeySocketException e) {
+                Timber.tag("CodeGrantAsyncTask")
+                        .d(e, "error processing the code grant");
+            }
         }
+        return null;
     }
 }
