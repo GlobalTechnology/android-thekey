48d8802471a65d5c98886abaa39c969f2720bf6f
Daniel Frett <daniel.frett@ccci.org>
2018-06-19 13:50:04 -0400

    add a getAttributes method that returns a future that will resolve to fresh attributes.

--- thekey-api/src/main/java/me/thekey/android/TheKeyAttributeApi.java
+++ thekey-api/src/main/java/me/thekey/android/TheKeyAttributeApi.java
@@ -1,9 +1,13 @@
 package me.thekey.android;
 
+import android.os.AsyncTask;
 import android.support.annotation.AnyThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 import me.thekey.android.exception.TheKeySocketException;
 
@@ -57,4 +61,32 @@ interface TheKeyAttributeApi extends TheKeySessions {
     @NonNull
     @AnyThread
     Attributes getCachedAttributes(@Nullable String guid);
+
+    /**
+     * This method will return attributes for the specified user.
+     * This method attempts to use the cached attributes, if the cached attributes are not valid or are stale we attempt
+     * to load fresh attributes before populating the returned future.
+     * This method is non-blocking, but the returned Future is blocking.
+     *
+     * @param guid The user we are looking for attributes for.
+     * @return a Future that will eventually resolve to the user's attributes.
+     */
+    @NonNull
+    @AnyThread
+    default Future<Attributes> getAttributes(@Nullable final String guid) {
+        final FutureTask<Attributes> task = new FutureTask<>(() -> {
+            final Attributes attributes = getCachedAttributes(guid);
+
+            // refresh attributes if they aren't valid or are stale
+            if (!attributes.areValid() || attributes.areStale()) {
+                loadAttributes(guid);
+                return getCachedAttributes(guid);
+            }
+
+            return attributes;
+        });
+        AsyncTask.THREAD_POOL_EXECUTOR.execute(task);
+
+        return task;
+    }
 }
